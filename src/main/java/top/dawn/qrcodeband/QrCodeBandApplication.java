cccd8f8c957e11916f1c8047f07a6bb525dcc373
ruffianjiang <jiangyj0516@outlook.com>
2021-10-19 16:50:57 +0800

    init

---
+++ src/main/java/top/dawn/qrcodeband/QrCodeBandApplication.java
@@ -0,0 +1,13 @@
+package top.dawn.qrcodeband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeBandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCodeBandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/dawn/qrcodeband/QrCodeBandApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dawn.qrcodeband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeBandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
